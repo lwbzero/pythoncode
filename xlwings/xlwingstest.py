7ba9ff38ef4d1beaf902a4a0bb585716b8a5fbea
lwb_zero <lwb_zero@163.com>
2021-09-07 19:03:24 +0800

    修改行高

--- xlwings/xlwingstest.py
+++ xlwings/xlwingstest.py
@@ -1,5 +1,4 @@
 #导入xlwings模块
-from os import truncate
 import xlwings as xw
 
 # 打开Excel程序，默认设置：程序可见，只打开不新建工作薄，屏幕更新关闭
@@ -11,25 +10,40 @@ app.screen_updating=False
 # wb=xw.books.active
 
 # 文件位置：filepath，打开test文档，然后保存，关闭，结束程序
-filepath=r'E:\\pythoncode\\xlwings\\dangbei358.xlsx'
+filepath=r'E:\\pythoncode\\pythoncode\\xlwings\\mac_data.xlsx'
 wb=app.books.open(filepath)
-wb.sheets.add(name='woshisheet2',before=None,after=None)#添加一个新的工作表
+# wb.sheets.add(name='woshisheet2',before=None,after=None)#添加一个新的工作表
 wb.sheets['sheet1'].range('A1').value='人生'#给sheet1的A1写入value
-wb.sheets['sheet2'].range('A1').value='苦短'
+# wb.sheets['woshisheet2'].range('A1').value='苦短'
+# wb.sheets['sheet3'].range('A1').value='苦短'
+
+# 示例代码
+wb = xw.books['mac_data.xlsx']
+wb = xw.books.active  #引用
+sht = xw.books['mac_data.xlsx'].sheets['sheet1']
+sht = xw.sheets.active
+rng=xw.books['mac_data.xlsx'].sheets['sheet1'].range('A1')
+height = 15
+wb.sheets[0]['1:10'].api.RowHeight = 100 #设置行高100
+# rng=xw.Range('A1')
+
+#workbook api
+# wbpath = wb.fullname
+# print(wbpath)
+# print(wb.name)
+
+#sheet api
+# sht = xw.books['workbook name'].sheets['sheet name']
+# sht = sht.active()
+# sht = sht.clear()
+# sht = sht.contents()
+# shtname = sht.name
+# sht.delete
+
+
+# rng = xw.Range('A1')
+rng.resize(row_size=350,column_size=None)
+
 wb.save()
 wb.close()
 app.quit()
-
-# 示例代码
-# wb = xw.books['工作簿的名字']
-# wb = xw.books.active  #引用
-# sht = xw.books['工作簿的名字'].sheets['sheet的名字']
-# sht = xw.sheets.active
-# rng=xw.books['工作簿的名字'].sheets['sheet的名字'].range('A1')
-# rng=xw.Range('A1')
-
-
-
-
-
-
